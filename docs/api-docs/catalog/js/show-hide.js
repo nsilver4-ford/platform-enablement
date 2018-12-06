430bb50e64e453f07e9f3fe70b9f878026102244
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-06 09:11:57 -0500

    Update show-hide.js

--- docs/api-docs/catalog/js/show-hide.js
+++ docs/api-docs/catalog/js/show-hide.js
@@ -1,16 +1,16 @@
-var $button = $('#myButton'),
+ var $button = $('#myButton'),
   $text   = $('#myText'),
-  visible = false;
+  visible = true;
 
 $button.click(function(){
 if ( visible ) {
   $text.slideUp('fast',function(){
-    $text.addClass('bomb-hide')
+    $text.addClass('hide')
          .slideDown(0);
   });
 } else {
   $text.slideUp(0,function(){
-    $text.removeClass('bomb-hide')
+    $text.removeClass('hide')
          .slideDown('fast');
   });
 }
