1e5ce11b207fdfb684ba640eca2e09905855386e
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-06 11:14:26 -0500

    Update show-hide.js

--- docs/api-docs/catalog/js/show-hide.js
+++ docs/api-docs/catalog/js/show-hide.js
@@ -1,20 +1,18 @@
-document.write("Hello World!");
-
 var $button = $('#myButton'),
-  $text   = $('#myText'),
-  visible = true;
+    $text   = $('#myText'),
+    visible = true;
 
-$button.click(function(){
-if ( visible ) {
-  $text.slideUp('fast',function(){
-    $text.addClass('hide')
-         .slideDown(0);
-  });
-} else {
-  $text.slideUp(0,function(){
-    $text.removeClass('hide')
-         .slideDown('fast');
-  });
-}
-visible = ! visible;
+$button.click(function() {
+  if (visible) {
+    $text.slideUp('fast',function() {
+      $text.addClass('hide')
+           .slideDown(0);
+    });
+  } else {
+    $text.slideUp(0,function() {
+      $text.removeClass('hide')
+           .slideDown('fast');
+    });
+  }
+  visible = !visible;
 });
