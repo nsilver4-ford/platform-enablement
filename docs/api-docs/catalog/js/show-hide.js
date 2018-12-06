0756187e41d3f2e5aab57d6116cde22e8a385969
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-06 13:26:28 -0500

    Update show-hide.js

--- docs/api-docs/catalog/js/show-hide.js
+++ docs/api-docs/catalog/js/show-hide.js
@@ -1,10 +1,15 @@
+
 $(document).ready(function(){
   $('#eventDescriptions>div').hide();
   $('#eventTitles a').click(function(){
     var target = $(this).attr("rel");
-    $(target).show('slow');
+    $(target).show('fast');
+    $('#eventTitles a').hide();
+    $('#eventDescriptions a.close').show();
   });
+
   $('#eventDescriptions a.close').click(function(){
-    $(this).parent().parent().hide('slow');
+    $(this).parent().parent().hide('fast');
+    $('#eventTitles a').show();
   })
 });
