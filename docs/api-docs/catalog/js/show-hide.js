87d1c9b3b0b456a5a65c0c840d275864c9232323
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-06 07:57:04 -0500

    Create show-hide.js

---
+++ docs/api-docs/catalog/js/show-hide.js
@@ -0,0 +1,18 @@
+var $button = $('#myButton'),
+  $text   = $('#myText'),
+  visible = true;
+
+$button.click(function(){
+if ( visible ) {
+  $text.slideUp('fast',function(){
+    $text.addClass('hide')
+         .slideDown(0);
+  });
+} else {
+  $text.slideUp(0,function(){
+    $text.removeClass('hide')
+         .slideDown('fast');
+  });
+}
+visible = ! visible;
+});
