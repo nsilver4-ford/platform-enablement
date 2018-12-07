13899c208fcb900bbc88c198cf3ff2e633004017
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-07 07:52:14 -0500

    Update show-hide.js

--- docs/api-docs/catalog/js/show-hide.js
+++ docs/api-docs/catalog/js/show-hide.js
@@ -1,11 +1,10 @@
-
 $(document).ready(function(){
   $('#eventDescriptions>div').hide();
   $('#eventTitles a').click(function(){
     var target = $(this).attr("rel");
     $(target).show('fast');
-    $('#eventTitles a').hide();
-    $('#eventDescriptions a.close').show();
+    $(this).hide("span");
+    $('#eventDescriptions a.close').show("fast");
   });
 
   $('#eventDescriptions a.close').click(function(){
@@ -13,3 +12,19 @@ $(document).ready(function(){
     $('#eventTitles a').show();
   })
 });
+
+
+//$(document).ready(function(){
+  //$('#eventDescriptions>div').hide();
+  //$('#eventTitles a').click(function(){
+    //var target = $(this).attr("rel");
+    //$(target).show('fast');
+    //$('#eventTitles a').hide();
+    //$('#eventDescriptions a.close').show();
+  //});
+
+//  $('#eventDescriptions a.close').click(function(){
+  //  $(this).parent().parent().hide('fast');
+    //$('#eventTitles a').show();
+ // })
+//});
