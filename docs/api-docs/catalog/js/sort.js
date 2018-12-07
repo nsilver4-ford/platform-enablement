3bc5303e9770849f56202addad2e38a7677d9aed
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-07 09:25:25 -0500

    Update sort.js

--- docs/api-docs/catalog/js/sort.js
+++ docs/api-docs/catalog/js/sort.js
@@ -1,9 +1,9 @@
 var $btns = $('.btn').click(function() {
   if (this.id == 'all') {
-    $('#parent > div').fadeIn(450);
+    $('#team > div').fadeIn(450);
   } else {
     var $el = $('.' + this.id).fadeIn(450);
-    $('#parent > div').not($el).hide();
+    $('#team > div').not($el).hide();
   }
   $btns.removeClass('active');
   $(this).addClass('active');
