a622f7d01d82ee3d1b6e57ae7cfa407ed4ee09d2
nsilver4-ford <45241826+nsilver4-ford@users.noreply.github.com>
2018-12-14 11:02:00 -0500

    Update sort.js

--- docs/api-docs/catalog/js/sort.js
+++ docs/api-docs/catalog/js/sort.js
@@ -1,10 +1,42 @@
-var $btns = $('.btn').click(function() {
-  if (this.id == 'all') {
-    $('#team > div').fadeIn(450);
-  } else {
-    var $el = $('.' + this.id).fadeIn(450);
-    $('#team > div').not($el).hide();
+filterSelection("all")
+function filterSelection(team) {
+  var x, i;
+  x = document.getElementsByClassName("catalog");
+  if (team == "all") team = "";
+  for (i = 0; i < x.length; i++) {
+    RemoveCssClass(x[i], "show");
+    if (x[i].className.indexOf(team) > -1) AddCssClass(x[i], "show");
   }
-  $btns.removeClass('active');
-  $(this).addClass('active');
-});
+}
+
+function AddCssClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function RemoveCssClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("buttnicks");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
